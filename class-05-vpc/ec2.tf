f95476a89653ef8d01faaa5fc2087026b4eaaa86
varunpandianp <varunpandian29@gmail.com>
2026-02-01 21:36:28 +0530

    vpc variable tf code update

---
+++ class-05-vpc/ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "webserver01" {
+  ami               = var.ami
+  instance_type     = var.instancetype
+  key_name          = "skoda-keypair"
+  security_groups   = ["skoda-sg"]
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = var.tag
+    Env  = "dev"
+  }
+}
---
+++ class-05-vpc/variables.tf
@@ -0,0 +1,11 @@
+variable "ami" {
+  default = "ami-0e2ba60a049853bb4"
+}
+
+variable "instancetype" {
+  default = "t3.micro"
+}
+
+variable "tag" {
+  default = "New-Server"
+}
