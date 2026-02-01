308ba59d3388ede767d2fb8c26d7427d147e2fb3
varunpandianp <varunpandian29@gmail.com>
2026-02-01 13:40:35 +0530

    s3 bucket and versoning life cycle policy

---
+++ class-04-s3-bucket/s3bucket.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
